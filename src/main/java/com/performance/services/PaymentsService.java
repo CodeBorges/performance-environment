d72c197d730ab34251f308c009a5219915707954
joao.borges <joao.borges@accurate.com.br>
2026-03-19 22:15:57 -0300

    feat: adding a micrometer to collect measurements

--- src/main/java/com/performance/services/PaymentsService.java
+++ src/main/java/com/performance/services/PaymentsService.java
@@ -5,13 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.performance.entity.Payments;
 import com.performance.producer.KafkaProducer;
 import com.performance.repository.PaymentsRepository;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
-@Service
+@ApplicationScoped
 public class PaymentsService {
 
     @Inject
@@ -23,17 +25,29 @@ public class PaymentsService {
     @Inject
     private ObjectMapper objectMapper;
 
+    @Inject
+    private MeterRegistry meterRegistry;
+
     @Transactional
     public Payments createPayment() throws JsonProcessingException {
+        Timer.Sample totalSample = Timer.start(meterRegistry);
+
         Payments payments = new Payments();
         payments.amount = 100L;
         payments.createdAt = LocalDateTime.now();
         payments.status = "CREATED";
 
+        Timer.Sample dbSample = Timer.start(meterRegistry);
         paymentsRepository.persist(payments);
-        String payment = objectMapper.writeValueAsString(payments);
-        kafkaProducer.send(payment);
+        dbSample.stop(meterRegistry.timer("payment.db.time","type", "database"));
 
+        String payment = objectMapper.writeValueAsString(payments);
+
+        Timer.Sample kafkaSample = Timer.start(meterRegistry);
+        kafkaProducer.send(payment);
+        kafkaSample.stop(meterRegistry.timer("payment.kafka.time","type", "kafka"));
+
+        totalSample.stop(meterRegistry.timer("payment.total.time", "type", "total"));
         return payments;
     }
 }
